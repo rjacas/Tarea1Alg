706a3d402d4d8e070b6064ed86d076569b35a605
Victor Rojas <vitorohe@gmail.com>
2011-12-07 23:17:12 -0300

    arreglos y tamanhos de archivo

--- src/algorithms/samplesort.c
+++ src/algorithms/samplesort.c
@@ -4,8 +4,7 @@ int samples[m];
 int keys[k];
 int files[k];
 char name_file[50];
-/* podria haber un arreglo para guardar tamano 
- * final de cada archivo, asi es mas facil ordenar*/
+int sizes[k];
 
 void samplesort(int fd){
 
@@ -13,26 +12,37 @@ void samplesort(int fd){
 
 	fill_samples(samples,fd);
 
+
+	/*limite min*/
 	keys[0] = 0;
 
-	for(i = 1; i < k; i++ ){
-		
+	/*iniciamos pivotes*/
+	for(i = 1; i < k; i++)
 		keys[i] = samples[(a + 1)*i];
-		
-	}
 
-	quickaux(0,k-2,keys);
 
+	/*ordenar pivotes*/
+	quickaux(0,k-1,keys);
+
+
+	/*crear archivos*/
 	for(i = 0; i < k ; i++ ){
+		sizes[i]=0;
 		memset(&name_file[0],0,sizeof(name_file));
 		sprintf(name_file, "buffer%d\n", i);
-		files[i] = open(name_file[i], O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
+		files[i] = open(name_file[i], O_RDWR | O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
+	}
+	
+	/*escribimos pivotes en sus archivos correspondientes*/
+	for(i = 1; i < k; i++){
+		write(files[i-1], keys[i], sizeof(int));
+		sizes[i-1]++;
 	}
 	
 	fill_files();
 	
 	/*¿sera necesario?*/
-	for( i = 0 ; i < k ; i++ )
+	for(i = 0 ; i < k ; i++)
 		close(files[i]);
 	
 	sort_files();
@@ -49,7 +59,7 @@ void fill_files(){
 	
 	int i,ini,fin,me,valor,temp;
 	
-	for(i = 0; i < m ; i++ ){
+	for(i = 0; i < m ; i++){
 		
 		valor = samples[i];
 			
@@ -59,13 +69,21 @@ void fill_files(){
 			
 		if(valor > keys[k-1]){
 				write(files[k-1], valor, sizeof(int));
+				sizes[k-1]++;
 				continue;
 		}
 			
 		while(1){
 			
-			if(((fin - ini) == 1) || (valor == keys[me])){
+			if((fin - ini) == 1){
 				write(files[ini], valor, sizeof(int));
+				sizes[ini]++;
+				break;
+			}
+			
+			if(valor == keys[me]){
+				write(files[ini], valor, sizeof(int));
+				sizes[me]++;
 				break;
 			}
 		
