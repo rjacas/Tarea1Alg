a140ff5e5e2d9ea548b3e71730bc8ab45af94d5b
jbahamon <jbahamon@ug.uchile.cl>
2011-12-12 21:14:52 -0200

    Update src/scripts/run_mergesort.sh

--- src/scripts/run_mergesort.sh
+++ src/scripts/run_mergesort.sh
@@ -1,1 +1,33 @@
-lalala
+#!/bin/bash
+
+k=11
+M=104857600
+max_n=10
+n_it=$((max_n-1))
+max_size=$((2**max_n * M))
+
+if [ ! -e "./rand_src" ]; then
+  echo "Creating file with $max_size bytes"
+	./make_test_file $max_size
+	mv ./test_file ./rand_src
+fi
+
+for i in `seq 1 $n_it`;
+do
+    rm -f test_file
+
+    test_size=$((2**i * M))
+    
+    echo "Creating file of size $test_size bytes; mod = $((test_size % 512))"
+    offset=$((((RANDOM % 100) * (max_size - test_size))/51200))
+
+    blocks=$((test_size/512))
+    dd if=./rand_src of=test_file bs=512 skip=$offset count=$blocks
+
+    rm -f aux
+    touch aux
+
+    echo "Testing with 2**$i * $M = $test_size bytes"
+    ./test_mergesort "test_file" $test_size aux $k
+    rm test_file
+done
